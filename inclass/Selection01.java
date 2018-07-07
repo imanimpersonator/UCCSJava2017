cbb02b468d433cccd9897c8052474475bdbeb9d8
Mitchell S <32205080+imanimpersonator@users.noreply.github.com>
2018-07-07 12:07:18 -0600

    Add files via upload

---
+++ inclass/Selection01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; 
+
+public class Selection01 {
+	public static void main (String[] args) {
+		// System.out.println("Hello World!");
+		
+		Scanner james = new Scanner(System.in);
+		System.out.println("Please print me a double ");
+		double d = james.nextDouble(); // can be any name ahead of .nextDouble()
+		james.close();
+		
+		int k = 3/2; // Would round down to 1
+		int i = 0;
+		i += 2; // i = i + 2;
+		i ++; // i = i + 1; i += 1;
+		i = 1;
+		++ i; // increment i first
+		int j = 0;
+		
+		j = i + 2;
+		i ++;
+		
+		
+		
+		
+	}
+}
